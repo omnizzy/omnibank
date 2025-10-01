47894123babd41a69169c2e882be8453f8d46933
omnizzy <118431825+omnizzy@users.noreply.github.com>
2025-10-01 04:32:34 +0100

    Swaggar Documentation

--- src/main/java/com/omnigroup/omnibank/OmnibankApplication.java
+++ src/main/java/com/omnigroup/omnibank/OmnibankApplication.java
@@ -1,9 +1,34 @@
 package com.omnigroup.omnibank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Omnibank Bank App",
+                description = "Backend Rest APIs for Omnibank",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Nathan Adebesin",
+                        email = "nathanadebesin@gmail.com",
+                        url = "https://github.com/omnizzy/omnibank"
+                ),
+                license = @License(
+                        name = "Omnibank",
+                        url = "https://github.com/omnizzy/omnibank"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Omnibank Documentation",
+                url = "https://github.com/omnizzy/omnibank"
+        )
+)
 public class OmnibankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/omnigroup/omnibank/controller/UserController.java
+++ src/main/java/com/omnigroup/omnibank/controller/UserController.java
@@ -2,41 +2,93 @@ package com.omnigroup.omnibank.controller;
 
 import com.omnigroup.omnibank.dto.*;
 import com.omnigroup.omnibank.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creates a new user and assigns a unique account ID."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping("/createaccount")
     public BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
     }
 
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Checks the balance of a user given their account number."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/balanceenquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request) {
         return userService.balanceEnquiry(request);
     }
 
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Retrieves the account holder's name given an account number."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/nameenquiry")
     public String nameEnquiry(@RequestBody EnquiryRequest request) {
         return userService.nameEnquiry(request);
     }
 
+    @Operation(
+            summary = "Credit Account",
+            description = "Credits (adds funds to) a user's account."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/credit")
     public BankResponse creditAccount(@RequestBody CreditDebitRequest request) {
         return userService.creditAccount(request);
     }
 
+    @Operation(
+            summary = "Debit Account",
+            description = "Debits (removes funds from) a user's account."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/debit")
     public BankResponse debitAccount(@RequestBody CreditDebitRequest request) {
         return userService.debitAccount(request);
     }
 
+    @Operation(
+            summary = "Transfer Funds",
+            description = "Transfers funds from one account to another."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/transfer")
     public BankResponse transfer(@RequestBody TransferRequest request) {
         return userService.transfer(request);
