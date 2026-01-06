926452612291e4697af205df84c1f73b90299898
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-01-06 19:19:17 +0100

    config

--- src/main/java/com/omnigroup/omnibank/config/JwtTokenProvider.java
+++ src/main/java/com/omnigroup/omnibank/config/JwtTokenProvider.java
@@ -1,8 +1,20 @@
 package com.omnigroup.omnibank.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt-expiration}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+        String Username =
+    }
 }
